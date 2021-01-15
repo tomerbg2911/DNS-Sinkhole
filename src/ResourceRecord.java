fde7e2e5009fb013a8970b9200ce422f3fa35032
Tomer <tomer2911@gmail.com>
2021-01-15 16:40:39 +0200

    Create ResourceRecord.java

---
+++ src/ResourceRecord.java
@@ -0,0 +1,11 @@
+
+public class ResourceRecord {
+    private String name;
+    private int type;
+
+    public ResourceRecord(String name, int type) {
+        this.name = name;
+        this.type = type;
+    }
+
+}
