033d4334b3350aa7d720edd9558d1ca53ea1cff8
Tomer <tomer2911@gmail.com>
2021-01-15 16:40:59 +0200

    Update DNSQuery.java

--- src/DNSQuery.java
+++ src/DNSQuery.java
@@ -16,77 +16,76 @@ public class DNSQuery {
 
     // RRs
     private Question question;
+    private ResourceRecord[] answerRRs;
+    private ResourceRecord[] authorityRRs;
 
     // section sizes
     private final int headerSize = 12;
-    private final int questionTypeSize = 2;
-    private final int questionClassSize = 2;
+    private final int TypeSize = 2;
+    private final int ClassSize = 2;
+    private final int rrTTLSize = 4;
+    private final int rrRDLength = 2;
 
-    // current position in bytes arry
+    // current position in bytes array
     private int bytesArrayCursor = 0;
 
     public DNSQuery(byte[] dnsQueryBytes) {
         this.dnsQueryBytes = dnsQueryBytes;
         showBytesAsBits(dnsQueryBytes);
-        
-        // read Header section
+
+        // analyze dns query sections while moving the cursor respectively
+        analyzeHeaderSection();
+        analyzeQuestionSection();
+        analyzeRRsSection();
+
+        // move cursor to the first byte
+        this.bytesArrayCursor = 0;
+    }
+
+    private void analyzeHeaderSection() {
         initFlags();
         initCounters();
         this.bytesArrayCursor += headerSize;
+    }
 
-        // read Question section
-        String questionName = readMsgFromByte(this.bytesArrayCursor, true);
+    private void analyzeQuestionSection() {
+        String questionName = readNameFromByte(this.bytesArrayCursor, true);
         byte[] questionTypeBytes = { dnsQueryBytes[this.bytesArrayCursor], dnsQueryBytes[this.bytesArrayCursor + 1] };
         int questionType = getNumberFromBytesArray(questionTypeBytes);
-        this.bytesArrayCursor += 2;
+        this.bytesArrayCursor += TypeSize;
+        this.bytesArrayCursor += ClassSize;
         this.question = new Question(questionName, questionType);
-        int a = 5;
     }
 
-    private void initRRs() {
-
+    private void analyzeRRsSection() {
+        // read RRs (answer + authorative) sections
+        if (this.answersCounter > 0) {
+            this.answerRRs = new ResourceRecord[this.answersCounter];
+            initRRArray(this.answerRRs);
+        } else if (this.authorativeCounter > 0) {
+            this.authorityRRs = new ResourceRecord[this.authorativeCounter];
+            initRRArray(this.authorityRRs);
+        }
     }
 
-    private String readMsgFromByte(int startingByteIdx, boolean advanceCursor) {
-
-        StringBuilder result = new StringBuilder();
-
-        int currentByteIdx = startingByteIdx;
-        int bytesToRead = dnsQueryBytes[currentByteIdx];
-
-        while (dnsQueryBytes[currentByteIdx] != 0) {
-
-            boolean isPointer = !(getBitsSeqFromByte(this.dnsQueryBytes[currentByteIdx], 0, 2) == 0); // check if this
-                                                                                                      // byte
-                                                                                                      // represnts a pointer
-                                                                                                      // from
-                                                                                                      // first 2 bits
-            if (isPointer) {
-                byte[] pointerBytes = { dnsQueryBytes[currentByteIdx], dnsQueryBytes[currentByteIdx + 1] };
-                pointerBytes[0] &= 0x3f;
-                int pointerByteIdx = getNumberFromBytesArray(pointerBytes);
-                result.append(readMsgFromByte(pointerByteIdx, false));
-                currentByteIdx += 2;
-            } else {
-                bytesToRead = dnsQueryBytes[currentByteIdx];
-                currentByteIdx++;
-                // append next label
-                for (int i = 0; i < bytesToRead; i++) {
-                    char c = (char) dnsQueryBytes[currentByteIdx];
-                    result.append(c);
-                    currentByteIdx++;
-                }
-                if (dnsQueryBytes[currentByteIdx] != 0) {
-                    result.append('.');
-                }
-            }
+    private void initRRArray(ResourceRecord[] rrArray) {
+        for (int i = 0; i < rrArray.length; i++) {
+            // RR name
+            String rrName = readNameFromByte(this.bytesArrayCursor, true);
+            // RR type
+            byte[] rrTypeBytes = { dnsQueryBytes[this.bytesArrayCursor], dnsQueryBytes[this.bytesArrayCursor + 1] };
+            int rrType = getNumberFromBytesArray(rrTypeBytes);
+            this.bytesArrayCursor += TypeSize;
+            // RR class & ttl
+            this.bytesArrayCursor += ClassSize;
+            this.bytesArrayCursor += rrTTLSize;
+            // RR RDLENGTH
+            byte[] rdLengthBytes = { dnsQueryBytes[this.bytesArrayCursor], dnsQueryBytes[this.bytesArrayCursor + 1] };
+            int rdLength = getNumberFromBytesArray(rdLengthBytes);
+            this.bytesArrayCursor += rrRDLength + rdLength;
+            // construct new ResourceRecord
+            rrArray[i] = new ResourceRecord(rrName, rrType);
         }
-        
-        if (advanceCursor) {
-            this.bytesArrayCursor += currentByteIdx - startingByteIdx;
-        }
-
-        return result.toString();
     }
 
     private void initFlags() {
@@ -108,9 +107,51 @@ public class DNSQuery {
         this.authorativeCounter = getNumberFromBytesArray(authorativeBytes);
     }
 
-    // static methods
+    private String readNameFromByte(int startingByteIdx, boolean advanceCursor) {
 
-    private static int getBitsSeqFromByte(byte inputByte, int position, int amount) {
+        StringBuilder result = new StringBuilder();
+
+        int currentByteIdx = startingByteIdx;
+        int bytesToRead = dnsQueryBytes[currentByteIdx];
+
+        while (dnsQueryBytes[currentByteIdx] != 0) {
+
+            boolean isPointer = !(getBitsSeqFromByte(this.dnsQueryBytes[currentByteIdx], 0, 2) == 0); // check if this
+                                                                                                      // byte
+                                                                                                      // represnts a
+                                                                                                      // pointer
+                                                                                                      // from
+                                                                                                      // first 2 bits
+            if (isPointer) {
+                byte[] pointerBytes = { dnsQueryBytes[currentByteIdx], dnsQueryBytes[currentByteIdx + 1] };
+                pointerBytes[0] &= 0x3f;
+                int pointerByteIdx = getNumberFromBytesArray(pointerBytes);
+                result.append(readNameFromByte(pointerByteIdx, false));
+                currentByteIdx += 2;
+            } else {
+                bytesToRead = dnsQueryBytes[currentByteIdx];
+                currentByteIdx++;
+                // append next label
+                for (int i = 0; i < bytesToRead; i++) {
+                    char c = (char) dnsQueryBytes[currentByteIdx];
+                    result.append(c);
+                    currentByteIdx++;
+                }
+                if (dnsQueryBytes[currentByteIdx] != 0) {
+                    result.append('.');
+                }
+            }
+        }
+
+        if (advanceCursor) {
+            this.bytesArrayCursor += currentByteIdx - startingByteIdx + 1;
+        }
+
+        return result.toString();
+    }
+
+    // bytes helper methods
+    private int getBitsSeqFromByte(byte inputByte, int position, int amount) {
         int andMask = 0;
         for (int i = 0; i < amount; i++) {
             andMask = (andMask << 1) + 1;
@@ -119,7 +160,7 @@ public class DNSQuery {
         return inputByte & andMask;
     }
 
-    private static int getNumberFromBytesArray(byte[] arr) {
+    private int getNumberFromBytesArray(byte[] arr) {
         int result = 0;
         for (byte b : arr) {
             result = result << 8;
@@ -128,7 +169,7 @@ public class DNSQuery {
         return result;
     }
 
-    private static void showBytesAsBits(byte[] dnsQueryBytes) {
+    private void showBytesAsBits(byte[] dnsQueryBytes) {
         int counter = 0;
         for (byte b : dnsQueryBytes) {
             System.out.println(counter++);
